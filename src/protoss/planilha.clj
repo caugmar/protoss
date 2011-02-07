882aa25a271c76b43f897da485c2acc07442941e
Guto Marcicano <caugmar@gmail.com>
2011-02-07 20:40:04 -0200

    Leitura da planilha de dados implementada.

--- src/protoss/planilha.clj
+++ src/protoss/planilha.clj
@@ -13,3 +13,22 @@
         arq (File. "temperatura.ods")]
     (.saveAs (SpreadSheet/createEmpty modelo) arq)
     (OOUtils/open arq)))
+
+(defn ler-linha [planilha linha campos] 
+  (apply merge 
+    (map (fn [i] {(keyword (.toLowerCase (.getValueAt planilha 0 i))) (.getValueAt planilha linha i)})
+         (range campos))))
+
+(defn ler []
+  (let [arquivo (File. "dados.ods")
+        planilha (SpreadSheet/createFromFile arquivo)
+        empresas (.getTableModel (.getSheet planilha 0) 0 0)
+        lancamentos (.getTableModel (.getSheet planilha 1) 0 0)
+        empresas-lidas (remove (fn [registro] (= (:cliente registro) ""))
+                               (map (fn [i] (ler-linha empresas i 15))
+                                    (range (inc (.getRowCount empresas)))))
+        lancamentos-lidos (remove (fn [registro] (= (:cliente registro) ""))
+                                  (map (fn [i] (ler-linha lancamentos i 5))
+                                       (range (inc (.getRowCount lancamentos)))))]
+    [empresas-lidas lancamentos-lidos]))
+
