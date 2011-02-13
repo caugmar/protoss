90edd9642511f7e83ad765cba03c03994bb0b11a
Guto Marcicano <caugmar@gmail.com>
2011-02-13 21:17:43 -0200

    Movi todos os modelos para templates/. E aproveitei para escapar o $ em todos eles, que isso estava causando problemas de parsing no StringTemplate.
    --HG--
    rename : aviso.st => templates/aviso.st
    rename : lbm.st => templates/lbm.st
    rename : minister.st => templates/minister.st
    rename : recibos-aluguel.st => templates/recibos-aluguel.st

--- src/protoss/modelos.clj
+++ src/protoss/modelos.clj
@@ -1,8 +1,10 @@
 (ns protoss.modelos
   (:import [org.antlr.stringtemplate StringTemplateGroup]))
 
-(def modelos (StringTemplateGroup. "templates" "."))
+(def modelos (StringTemplateGroup. "templates" "./templates/"))
 (def aviso (.getInstanceOf modelos "aviso"))
 (def minister (.getInstanceOf modelos "minister"))
 (def lbm (.getInstanceOf modelos "lbm"))
+(def recibos-aluguel (.getInstanceOf modelos "recibos-aluguel"))
+(def recibos-escritorio (.getInstanceOf modelos "recibos-escritorio"))
 
